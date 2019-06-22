c494c1620a2964c5de245e083d8fccdebdf3e919
Andre Rua <1181689@isep.ipp.pt>
2019-06-22 23:20:01 +0100

    all

--- src/main/java/pt/ipp/isep/dei/project/io/ui/commandline/DataImporter.java
+++ src/main/java/pt/ipp/isep/dei/project/io/ui/commandline/DataImporter.java
@@ -83,10 +83,8 @@ public class DataImporter {
             e.printStackTrace();
         }
         inputHelperUI.acceptPathJSONorXMLAndReadFile("src/test/resources/geoAreaFiles/DataSet_sprint07_GA.json", geographicAreaRepository, sensorTypeRepository, areaTypeRepository);
-        gaSettingsUI.importReadingsFromJSON("src/test/resources/readingsFiles/DataSet_sprint07_GAData.json");
         readerController.readJSONAndDefineHouse(house, "src/test/resources/houseFiles/DataSet_sprint06_HouseData.json");
         houseConfigurationController.readSensors("src/test/resources/houseSensorFiles/DataSet_sprint07_HouseSensors.json");
-        houseConfigurationUI.importReadingsFromJSON("src/test/resources/readingsFiles/DataSet_sprint07_HouseSensorData.json");
         createUsers();
         System.out.println("...");
         System.out.println("Data imported successfully");
